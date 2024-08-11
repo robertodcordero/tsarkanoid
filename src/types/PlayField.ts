d06817558316c9c01bba0062525805954a1b98d2
Roberto Cordero <robertodcordero@hotmail.com>
2024-08-11 14:44:22 -0600

    Added pause/resume game.

--- src/types/PlayField.ts
+++ src/types/PlayField.ts
@@ -5,21 +5,10 @@ import { Ball } from '@/types/Ball'
 export class PlayField {
   _canvas: HTMLCanvasElement
   private _context: CanvasRenderingContext2D | null
-  private _scoreDisplay: HTMLDivElement | null
-  private _start: HTMLDivElement | null
-  private _info: HTMLDivElement | null
 
-  constructor(
-    canvas: HTMLCanvasElement,
-    score: HTMLDivElement | null,
-    start: HTMLDivElement | null,
-    info: HTMLDivElement | null
-  ) {
+  constructor(canvas: HTMLCanvasElement) {
     this._canvas = canvas
     this._context = this._canvas.getContext('2d')
-    this._scoreDisplay = score
-    this._start = start
-    this._info = info
   }
 
   get canvas(): HTMLCanvasElement {
@@ -28,22 +17,6 @@ export class PlayField {
 
   clear(): void {
     this._context?.clearRect(0, 0, this._canvas.width, this._canvas.height)
-  }
-
-  initStartButton(startFunction: (view: PlayField) => void): void {
-    this._start?.addEventListener('click', () => startFunction(this))
-  }
-
-  drawScore(score: number): void {
-    if (this._scoreDisplay) {
-      this._scoreDisplay.innerHTML = score.toString()
-    }
-  }
-
-  drawInfo(text: string): void {
-    if (this._info) {
-      this._info.innerHTML = text
-    }
   }
 
   drawSprite(sprite: Brick | Paddle | Ball): void {
